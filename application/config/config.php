e0b3aa36a0c6bf8cf4c116136a90b6182cd6b788
irshadillias <irshadillias@gmail.com>
2018-09-06 23:48:28 +0800

    add test

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8888/VideoChat/';
+$config['base_url'] = 'https://alitqanlogisticservices.com/VideochatDemo/';
 
 
 /*
